fd08b398ac00954a856b5a30e717a36a430e6343
beckyurbanic <72364922+beckyurbanic@users.noreply.github.com>
2020-12-07 21:53:39 -0500

    Add files via upload

---
+++ src/main/java/com/techelevator/Item.java
@@ -0,0 +1,65 @@
+package com.techelevator;
+
+import java.text.DecimalFormat;
+
+
+public class Item {
+	private int quantity = 5;
+	private String location;
+	private String name;
+	private double price;
+	private String type;
+	private static DecimalFormat df = new DecimalFormat("0.00");
+
+	
+	public Item(String location, String name, double price, String type) {
+		this.location = location;
+		this.name = name;
+		this.price = price;
+		this.type = type;
+	}	
+
+	public int dispense(int quantityToRemove) {
+			quantity -= quantityToRemove;
+			
+			
+			System.out.println();
+			System.out.println(name + " $" + df.format(price * quantityToRemove));
+			message();
+			System.out.println();
+			
+			return quantity;
+	}
+
+	public void message() {
+		
+	}
+
+	//Getters & Setters
+
+	public int getQuantity() {
+		return quantity;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public String getName() {
+		return name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public String getType() {
+		return type;
+	}
+
+
+
+
+
+
+
+
+	
+
+}
